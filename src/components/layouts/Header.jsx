416ba926e4fcf12788b3e1e18223d8fc14ef43e8
toyin <toyinmuhammed50@gmail.com>
2022-11-02 10:16:20 +0200

    Added some routes + change some styles + Added a db.json

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -27,8 +27,8 @@ const Header = () => {
     <>
       <div className="bg-white py-3 flex">
         <div className="flex flex-row-reverse gap-4 items-center mx-auto">
-          <a href='https://iua.edu.sd' target="_blank" className='text-md font-bold border-l-2 pl-4'>جـــامعـــــة إفـــــريقـــــيـــــا الـعــــــالمــــــيــــــة</a>
-          <Link to="/" className='text-md'>كلـــية الـدراســات الإســـلامـية</Link>
+          <a href='https://iua.edu.sd' target="_blank" className='text-lg font-bold border-l-2 pl-4'>جـــامعـــــة إفـــــريقـــــيـــــا الـعــــــالمــــــيــــــة</a>
+          <Link to="/" className='text-lg'>كلـــية الـدراســات الإســـلامـية</Link>
         </div>
       </div>
 
@@ -36,8 +36,13 @@ const Header = () => {
         <div className="flex flex-row-reverse items-center justify-between">
           <Link to="/" className="flex-row-reverse flex gap-2 items-center my-6">
             <img src="/logo.png" alt="logo_iua" className='w-8' />
-            <h1 className='text-2xl font-bold text-yellow-900'>الدراسات الإسلامية</h1>
+            <h1 className='text-3xl font-bold text-yellow-900'>الدراسات الإسلامية</h1>
           </Link>
+          {/**
+           * TODO
+           * - make texts more bolder
+           * - make logo bigger also
+           */}
           <div className="flex flex-row-reverse gap-1 items-center text-white subnav">
             <NavLink to="/" className="px-3 py-2 text-sm rounded-md hover:bg-yellow-200 hover:bg-opacity-50">الرئيسية</NavLink>
             <NavLink to="/about" className="px-3 py-2 text-sm rounded-md hover:bg-yellow-200 hover:bg-opacity-50">عن الكلية</NavLink>
@@ -45,6 +50,8 @@ const Header = () => {
             <NavLink to="/haiah" className="px-3 py-2 text-sm rounded-md hover:bg-yellow-200 hover:bg-opacity-50">هيئة التدريس</NavLink>
             <NavLink to="/haikal" className="px-3 py-2 text-sm rounded-md hover:bg-yellow-200 hover:bg-opacity-50">الهيكل الإداري</NavLink>
             <NavLink to="/muktamarat" className="px-3 py-2 text-sm rounded-md hover:bg-yellow-200 hover:bg-opacity-50">المؤتمرات</NavLink>
+            <NavLink to="/dirasat" className="px-3 py-2 text-sm rounded-md hover:bg-yellow-200 hover:bg-opacity-50">الـدراســات العليا</NavLink>
+            <NavLink to="/news" className="px-3 py-2 text-sm rounded-md hover:bg-yellow-200 hover:bg-opacity-50">الأخبار</NavLink>
             <div className="px-3 py-2 rounded-md hover:bg-yellow-200 hover:bg-opacity-50" >
               <FiSearch />
             </div>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
       <div className="flex flex-col font-bold gap-3 items-center mx-auto">
         <h1 className='text-6xl text-yellow-600'>الدراسات الإسلامية</h1>
         <p className='text-2xl '>الموقع الرسمي لكلية الدراسات الإسلامية بجامعة إفريقيا العالمية</p>
-        <p className='text-xl'>الخرطوم | السودان</p>
+        <p className='text-xl text-yellow-600'>الخرطوم | السودان</p>
 
         <div className="flex flex-row-reverse items-center gap-4 mt-14">
           <Link to="/programs" className='px-8 py-2 border border-yellow-600 hover:bg-yellow-500 bg-yellow-600 rounded-lg flex flex-row-reverse items-center gap-2'><FiBriefcase /> انظر البرامج</Link>
-          <Link to='/contact' className='px-8 py-2 border border-yellow-600 rounded-lg flex flex-row-reverse items-center gap-2'><FiPhone /> اتصل بنا</Link>
+          <Link to='/contact' className='px-8 py-2 border border-yellow-600 hover:bg-stone-300 hover:text-yellow-600 rounded-lg flex flex-row-reverse items-center gap-2'><FiPhone /> اتصل بنا</Link>
         </div>
       </div>
     </div>

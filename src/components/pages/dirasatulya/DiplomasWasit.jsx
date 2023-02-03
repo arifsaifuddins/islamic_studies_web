2a35f918ded84a5cb08d75c7a698902b6e97d29b
saifuddien <ariefsaifuddien01@gmail.com>
2023-02-04 00:16:25 +0200

    6

--- src/components/pages/dirasatulya/DiplomasWasit.jsx
+++ src/components/pages/dirasatulya/DiplomasWasit.jsx
@@ -181,7 +181,7 @@ const DiplomaWasit = () => {
               diploma_wasit_quran_5?.length > 0 && (
                 <>
                   <div className="mb-20">
-                    <h2 className='text-lg text-white w-full px-4 py-2 bg-yellow-600 text-center'>الفصل الدراسي الرابع</h2>
+                    <h2 className='text-lg text-white w-full px-4 py-2 bg-yellow-600 text-center'>الفصل الدراسي الخامس</h2>
                     <ul className='bg-white border-x'>
                       <li className='flex flex-row items-center font-bold gap-6 border-b-4 px-6 py-2 text-yellow-600'>
                         <p className='w-[65%] text-center'>المواد الدراسية</p>
@@ -215,7 +215,7 @@ const DiplomaWasit = () => {
               diploma_wasit_quran_6?.length > 0 && (
                 <>
                   <div className="mb-20">
-                    <h2 className='text-lg text-white w-full px-4 py-2 bg-yellow-600 text-center'>الفصل الدراسي الرابع</h2>
+                    <h2 className='text-lg text-white w-full px-4 py-2 bg-yellow-600 text-center'>الفصل الدراسي السادس</h2>
                     <ul className='bg-white border-x'>
                       <li className='flex flex-row items-center font-bold gap-6 border-b-4 px-6 py-2 text-yellow-600'>
                         <p className='w-[65%] text-center'>المواد الدراسية</p>
@@ -392,7 +392,7 @@ const DiplomaWasit = () => {
               diploma_wasit_dakwah_5?.length > 0 && (
                 <>
                   <div className="mb-20">
-                    <h2 className='text-lg text-white w-full px-4 py-2 bg-yellow-600 text-center'>الفصل الدراسي الرابع</h2>
+                    <h2 className='text-lg text-white w-full px-4 py-2 bg-yellow-600 text-center'>الفصل الدراسي الخامس</h2>
                     <ul className='bg-white border-x'>
                       <li className='flex flex-row items-center font-bold gap-6 border-b-4 px-6 py-2 text-yellow-600'>
                         <p className='w-[65%] text-center'>المواد الدراسية</p>
@@ -426,7 +426,7 @@ const DiplomaWasit = () => {
               diploma_wasit_dakwah_6?.length > 0 && (
                 <>
                   <div className="mb-20">
-                    <h2 className='text-lg text-white w-full px-4 py-2 bg-yellow-600 text-center'>الفصل الدراسي الرابع</h2>
+                    <h2 className='text-lg text-white w-full px-4 py-2 bg-yellow-600 text-center'>الفصل الدراسي السادس</h2>
                     <ul className='bg-white border-x'>
                       <li className='flex flex-row items-center font-bold gap-6 border-b-4 px-6 py-2 text-yellow-600'>
                         <p className='w-[65%] text-center'>المواد الدراسية</p>

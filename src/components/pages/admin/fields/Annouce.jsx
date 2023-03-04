6fa73fb348102953fc8c0bc42809bab2cf065eb9
ariefsaifuddien <ariefsaifuddien01@gmail.com>
2023-03-04 23:23:48 +0200

    pl

--- src/components/pages/admin/fields/Annouce.jsx
+++ src/components/pages/admin/fields/Annouce.jsx
@@ -71,7 +71,7 @@ function AnnoucField({ url }) {
         (Poster != null) && <img src={URL.createObjectURL(Poster)} alt="blog" className="mt-4 w-full" />
       }
       <div className="my-5">
-      <p className="cursor-pointer text-center bg-yellow-600 text-white py-1 pl-3 rounded-lg hover:bg-yellow-700 font-bold w-[100%]" onClick={() => submitAnnouc()}>إضافة الإعلام</p>
+        <p className="cursor-pointer text-center bg-yellow-600 text-white py-1 pl-3 rounded-lg hover:bg-yellow-700 font-bold w-[100%]" onClick={() => submitAnnouc()}>إضافة الإعلام</p>
       </div>
     </>
   )
